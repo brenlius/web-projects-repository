314ec06127091457b42b0b78de9a347d23a10e20
Bren Lius Mendoza <brenliusmendoza@gmail.com>
2025-04-21 19:02:32 +0800

    [DEV][Release 1.1.1] - created default js file

---
+++ Web.Profile.Bren.UI/wwwroot/lib/default/js/app.js
@@ -0,0 +1,10 @@
+﻿document.querySelector('#body-content').addEventListener('scroll', function () {
+    const navbar = document.getElementsByClassName('navbar');
+    const content = document.getElementsByClassName('content');
+
+    //if (content[0].scrollTop > 80) {
+    //    document.getElementsByClassName('navbar').style.padding = "30px 10px";
+    //} else {
+    //    document.getElementsByClassName('navbar').style.padding = "80px 10px";
+    //}
+});

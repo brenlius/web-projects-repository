a8f8b7201c6db712d736f3792518425adfdee59c
brenlius <brenliusmendoza@gmail.com>
2025-05-01 15:13:50 +0800

    [DEV][5.1.1] - added jquery and navigation bar dropdown menu sliding effect

--- Web.Profile.Bren.UI/wwwroot/lib/default/js/app.js
+++ Web.Profile.Bren.UI/wwwroot/lib/default/js/app.js
@@ -1,10 +1,22 @@
-﻿document.querySelector('#body-content').addEventListener('scroll', function () {
-    const navbar = document.getElementsByClassName('navbar');
-    const content = document.getElementsByClassName('content');
+﻿$(window).on('load', function () {
+    //listen for the Bootstrap dropdown 'show' event
+    $('.dropdown').on('show.bs.dropdown', function (e) {
+        //first the dropdown menu within the current dropdown
+        //use .first() in case of nested dropdowns or other elements
+        $(this).find('.dropdown-menu').first().stop(true, true).slideDown(300);
+    });
 
-    //if (content[0].scrollTop > 80) {
-    //    document.getElementsByClassName('navbar').style.padding = "30px 10px";
-    //} else {
-    //    document.getElementsByClassName('navbar').style.padding = "80px 10px";
-    //}
+    //listen for the Bootstrap dropdown 'hide' event
+    $('.dropdown').on('hide.bs.dropdown', function (e) {
+        //prevent the default Bootstrap hide behavior (which is instant)
+        e.preventDefault();
+
+        //find the dropdown menu and slide it up
+        //use a callback function to remove the 'show' class after the animation
+        $(this).find('.dropdown-menu').first().stop(true, true).slideUp(300, function () {
+            //'this' inside the callback refers to the dropdown-menu element
+            //get the parent (.dropdown) and remove the 'show' class
+            $(this).removeClass('show');
+        });
+    });
 });
